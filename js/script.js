a1c0b3ebdb0ef594c65b8b8d6660f841e4336565
davidcassaldesign <61119118+davidcassaldesign@users.noreply.github.com>
2021-01-15 21:32:45 -0300

    Desenvolvimento
    Animações na página inicial

--- js/script.js
+++ js/script.js
@@ -38,3 +38,64 @@ lightModeToggle.addEventListener('click', () => {
     disableLightMode(); 
   }
 });
+
+// Animação Scroll
+const debounce = function(func, wait, immediate) {
+  let timeout;
+  return function(...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll() {
+  const windowTop = window.pageYOffset + ((window.innerHeight * 2) / 3);
+  target.forEach(function(element) {
+    if((windowTop) > element.offsetTop) {
+      element.classList.add(animationClass);
+    } else {
+      element.classList.remove(animationClass);
+    }
+  })
+}
+
+animeScroll();
+
+if(target.length) {
+  window.addEventListener('scroll', debounce(function() {
+    animeScroll();
+  }, 50));
+}
+
+// Efeito fade
+const pin = document.querySelectorAll('[data-opacity]');
+const fadeClass = 'fade-opacity';
+
+function fadeUp() {
+  const windowTop = window.onscroll + document.documentElement.scrollTop > 75;
+  pin.forEach(function(element) {
+    if((windowTop) > element.offsetTop) {
+      element.classList.add(fadeClass);
+    } else {
+      element.classList.remove(fadeClass);
+    }
+  })
+}
+
+fadeUp();
+
+if(pin.length) {
+  window.addEventListener('scroll', debounce(function() {
+    fadeUp();
+  }, 50));
+}
